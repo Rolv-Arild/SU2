5743dd97d968fb616016aedbe1dbbf9d71f36b75
Rolv-Arild Braaten <rolv_arild@hotmail.com>
2017-09-12 15:39:44 +0200

    Fullførte øving 4

---
+++ exercise4/src/main/java/KundeService/Kunde.java
@@ -0,0 +1,25 @@
+package KundeService;
+
+/**
+ * Created by Rolv-Arild on 12.09.2017.
+ */
+public class Kunde {
+    private String navn;
+    private String id;
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
---
+++ exercise4/src/main/java/KundeService/KundeService.java
@@ -0,0 +1,50 @@
+package KundeService;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/kunder/")
+public class KundeService {
+	private static Map<String,Kunde> kunder = new HashMap<>();
+
+    @GET
+    @Path("/{kundeId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Kunde getKunde(@PathParam("kundeId") String kundeId) {
+        if (!kunder.containsKey(kundeId)) throw new NotFoundException("Not found");
+        return kunder.get(kundeId);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Collection<Kunde> getKunder() {
+        return kunder.values();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addKunde(Kunde kunde) {
+        kunder.put(kunde.getId(), kunde);
+    }
+
+    @DELETE
+    @Path("/{kunder}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void deleteKunde(Kunde kunde) {
+        kunder.remove(kunde.getId());
+    }
+
+    @PUT
+//    @Path("/{kunder}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void endreKunde(Kunde kunde) {
+        if (kunder.get(kunde.getId()) != null) {
+            kunder.put(kunde.getId(), kunde);
+        } else {
+            throw new NotFoundException("Not found");
+        }
+    }
+}
